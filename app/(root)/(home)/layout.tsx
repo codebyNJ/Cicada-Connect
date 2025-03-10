45df417897dd72c6f15ff8c0f634a303f00712d8
codebyNJ <nijeesh10th@gmail.com>
2025-03-10 09:14:27 +0530

    second

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'Cicada Connect',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
